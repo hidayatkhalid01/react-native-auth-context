d7cca2f016b638c9557398f2e9b8417af5334667
hidayatkhalid01 <itsme.khairulhidayat@gmail.com>
2026-02-10 02:32:00 +0800

    Fix index page UI

--- app/index.tsx
+++ app/index.tsx
@@ -4,14 +4,15 @@ import { Text } from '@/components/ui/text';
 
 import { Button, ButtonText } from '@/components/ui/button';
 import { useRouter } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Home() {
   const router = useRouter();
   return (
-    <Box className='bg-background-300 flex items-center justify-center flex-1'>
-      <Text className="text-black text-3xl font-black">Welcome to My App</Text>
-      <Text>This is my demo app for login functionality. Please sign in</Text>
-      <Box className='flex flex-col gap-2'>
+    <SafeAreaView className='bg-white flex flex-1 p-5 justify-center items-center gap-5'>
+      <Text className="text-black text-5xl font-black text-center">Welcome to My App</Text>
+      <Text className='text-center text-lg'>This is my demo app for login functionality. Please proceed to sign in or create an account.</Text>
+      <Box className='flex flex-col gap-2 w-full'>
         <Button className="bg-primary-500" onPress={() => router.push('/login')}>
           <ButtonText>Sign In</ButtonText>
         </Button>
@@ -19,6 +20,6 @@ export default function Home() {
           <ButtonText>Sign Up</ButtonText>
         </Button>
       </Box>
-    </Box>
+    </SafeAreaView>
   );
 }
